4e423c17a06987b208a3a66302800dca0b93db97
Luis Inserra <luisinserra@gmail.com>
2018-03-29 13:14:54 -0300

    alterações

---
+++ www/js/config.js
@@ -0,0 +1,191 @@
+function initConfig(){
+	var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+		var pastaAtual=getPastaAtual();
+		document.getElementById('tPasta').value=pastaAtual;
+    }
+};
+}
+function getPastaAtual(){
+	if (window.localStorage.getItem('parmPasta') == null){
+		window.localStorage.setItem('parmPasta','/');
+	}
+	var pastaAtual=window.localStorage.getItem('parmPasta');
+	return pastaAtual;
+}
+function goBuscaPasta() {
+	var pastaAtual=document.getElementById('tPasta').value;
+	var pasta='file://'+pastaAtual;
+	alert("Pasta: "+pasta);
+/*	
+	window.resolveLocalFileSystemURI(pasta, pastaSucesso, pastaErro);
+	try {
+		window.resolveLocalFileSystemURI(pasta, pastaSucesso, pastaErro);
+	}  catch(e){
+		alert("Erro: "+e.message);
+	}
+*/
+
+	if (typeof LocalFileSystem === 'undefined') {
+	    var PERSISTENT = window.PERSISTENT;
+	} else {
+	    var PERSISTENT = LocalFileSystem.PERSISTENT ;
+	}
+
+	try {
+		window.requestFileSystem(PERSISTENT,0,  onFileSystemSuccess, onErrorRead);
+		alert("Passou passo 1");
+		window.resolveLocalFileSystemURI(pasta, onResolveSuccess, fail);
+		alert("Passou passo 2");
+	}  catch(e){
+		alert("Erro: "+e.message);
+	}
+
+	function onResolveSuccess(fileEntry) {
+		alert("resolvido.");
+        console.log(fileEntry.name);
+    }
+
+    function fail(evt) {
+        console.log(evt.target.error.code);
+    }
+	
+/*
+	try {
+		new ExternalStorageSdcardAccess( fileHandler ).scanPath( "pasta" );
+	} catch(e){
+		alert("Erro: "+e.message);
+	}
+    function fileHandler( fileEntry ) {
+        alert( fileEntry.name + " | " + fileEntry.toURL() );
+        document.getElementById('spanResposta').append(fileEntry.name+'<br>');
+    }
+*/    
+}
+function onFileSystemSuccess(fs) {
+    alert("Sucesso");
+    var pathInicial=fs.root.fullPath;
+    alert("Entrando com "+pathInicial+"...");
+    fs.root.fullPath = '/';
+    alert("mudou o path...");
+    var dirReader = fs.root.createReader();
+    alert("reader criado para ler de "+fs.root.fullPath+"...");
+    dirReader.readEntries(successRead,onErrorRead);
+}
+function successRead(entries){
+    alert("sucesso lendo");
+     var i;
+     var objectType;
+     var n=entries.length;
+     alert("varrendo "+n+"...");
+     for (i=0; i < entries.length; i++) {
+        if(entries[i].isDirectory == true) {
+            objectType = 'Directory';
+        } else {
+            objectType = 'File';
+            alert("arquivo");
+            alert(entries[i].name);
+        }
+        document.getElementById('spanResposta').append('<li><h3>' + entries[i].name + '</h3><p>' + entries[i].toURI() + '</p><p class="ui-li-aside">Type:<strong>' + objectType + '</strong></p></li>');
+    }
+    document.getElementById('spanResposta').listview("refresh");
+}
+
+function onErrorRead2(error) {
+    alert("Failed to list directory contents: " + error.code+","+error.message);
+}
+
+function pastaSucesso(entry){
+	alert("Iniciando o scan...");
+	if (entry.isFile) {
+		alert("Achei um arquivo...");
+            //fileHandler( entry );
+            var nome=entry.name;
+            var msg="arquivo "+nome;
+        } else {
+        	alert("Achei uma pasta...");
+        	var nome=entry.name;
+        	var msg="pasta "+nome;
+        }
+        var resultado=document.getElementById('spanResposta').innerHTML;
+        alert("já pegou um resultado...");
+        if (resultado != ''){
+        	resultado+='<br>';
+        }
+        resultado+=msg;
+        document.getElementById('spanResposta').innerHTML=resultado;
+}
+function pastaErro(erro){
+	alert("Erro acessando arquivos: "+erro.code+","+erro.message)
+}
+
+
+
+
+
+
+function startaLeitura(){
+    alert("vou ler...");
+     window.requestFileSystem(LocalFileSystem.PERSISTENT,0,  onFileSystemSuccess, onErrorRead);
+}
+
+function onFileSystemSuccess2(fs) {
+    alert("Sucesso");
+    var pathInicial=fs.root.fullPath;
+    alert("Entrando com "+pathInicial+"...");
+    fs.root.fullPath = '/mnt/sdcard';
+    alert("mudou o path...");
+    var dirReader = fs.root.createReader();
+    alert("reader criado para ler de "+fs.root.fullPath+"...");
+    dirReader.readEntries(successRead,onErrorRead);
+}
+
+function successRead(entries){
+    alert("sucesso lendo");
+     var i;
+     var objectType;
+     var n=entries.length;
+     alert("varrendo "+n+"...");
+     for (i=0; i < entries.length; i++) {
+        if(entries[i].isDirectory == true) {
+            objectType = 'Directory';
+        } else {
+            objectType = 'File';
+            alert("arquivo");
+            alert(entries[i].name);
+        }
+        $('#dirList').append('<li><h3>' + entries[i].name + '</h3><p>' + entries[i].toURI() + '</p><p class="ui-li-aside">Type:<strong>' + objectType + '</strong></p></li>');
+    }
+    $('#dirList').listview("refresh");
+}
+
+function onErrorRead(error) {
+    alert("Failed to list directory contents: " + error.code+","+error.message);
+}
---
+++ www/js/pn.js
@@ -0,0 +1,35 @@
+function goAcesso(){
+	db = window.openDatabase("DbPesqNotas", "1.0", "DbPesqNotas", 10);
+	db.transaction(setDB, erroOpen, sucesso); 
+}
+function erroOpen(erro){
+	alert("Erro set database..."+erro.code+":"+erro.message);
+}
+function sucesso(){
+	checaLogin();
+}
+function setDB(tx){
+	tx.executeSql('CREATE TABLE IF NOT EXISTS pesqnotas (id integer primary key, pasta text)');
+}
+
+function checaLogin(){
+    db.transaction(getRegistro, errorCheca);
+}
+function errorCheca(erro){
+	alert("Erro lendo registros..."+erro.code+":"+erro.message);
+}
+function getRegistro(tx){
+	tx.executeSql('SELECT * FROM pesqnotas', [], dbSucesso, dbErro);
+}
+function dbErro(erro){
+	alert("Erro lendo login..."+erro.code+":"+erro.message);
+}
+function dbSucesso(tx, results){
+	var n=results.rows.length;
+	var mensagem ="Temos "+n+" registros, saltar login...";
+	if (n == 0){
+		window.open('config.html','_top');
+	} else {
+		window.open('menu.html','_top');
+	}
+}
